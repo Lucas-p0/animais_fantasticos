a2a4398e111cb5d52876d12d9b3ecef4bd7eec83
Lucas pinheiro <lucaslogos@gmail.com>
2022-02-07 13:28:59 -0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const animais = document.getElementById('animais');
+const img = document.querySelectorAll('img');
+console.log(img);
+
+
+const imagensAnimais = querySelectorAll('img[src^="imagem"]');
+console.log(imagensAnimais);
+
+const linkInterno = document.querySelectorAll('[href^="#"]');
+console.log(linkInterno);
+
+const animais = document.querySelector('.animais-descricao');
+const h2Animais = document.querySelector('.animais-descricao h2');
+
+console.log(animais);
+console.log(h2Animais);
