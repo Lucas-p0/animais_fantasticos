f42aea11c9b68779ed33c80e0a2834181458bc0f
Lucas pinheiro <lucaslogos@gmail.com>
2022-02-16 13:16:08 -0300

    update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const img = document.querySelectorAll('img');
 console.log(img);
 
 
-const imagensAnimais = querySelectorAll('img[src^="imagem"]');
+const imagensAnimais = document.querySelectorAll.querySelectorAll('img[src^="imagem"]');
 console.log(imagensAnimais);
 
 const linkInterno = document.querySelectorAll('[href^="#"]');
@@ -13,3 +13,26 @@ const h2Animais = document.querySelector('.animais-descricao h2');
 
 console.log(animais);
 console.log(h2Animais);
+
+const itensMenu = document.querySelectorAll('.menu a')
+itensMenu.forEach((item) => {
+    item.classList.add('ativo');
+})
+itensMenu.forEach((item) => {
+    item.classList.remove('ativo');
+})
+itensMenu[0].classList.add('ativo')
+
+const imgs = ((img) => {
+    img.hasAttribute(img, alt);
+})
+document.querySelector('a[href^="http"]');
+linkInterno.setAttibute()
+
+
+const listaAnimais = document.querySelector('.animais-lista');
+
+const height = listaAnimais.scrollHeight;
+const animaisTop = listaAnimais.offsetTop;
+console.log(animaisTop);
+const h2left = primeiroh2.offsetLeft;
