4d882514165ada851d6e56db1cee85c8db952fcb
Lucas pinheiro <lucaslogos@gmail.com>
2022-03-16 13:23:54 -0300

    update

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ function somaImage() {
 window.onload = function () {
   somaImage();
 };
+
+const links = document.querySelectorAll("a");
+
+links.forEach((link) => {
+  const linkWidth = link.offsetWidth;
+  const linkHeight = link.offsetHeight;
+  if (linkWidth >= 48 && linkHeight >= 48) {
+    console.log(link, "Possui boa acessibilidade");
+  } else {
+    console.log(link, "Não possui boa acessibilidade");
+  }
+});
+
+const browerSmall = window.matchMedia("(max-width:720px)").matches;
+
+if (browerSmall) {
+  const menu = document.querySelector(".menu");
+  menu.classList.add("menu-mobile");
+}
